d0f3d3e9bd9adc87d5c1cc0b442c86c8bf0b919c
penda thiao <thiaopenda2503@gmail.com>
2024-12-13 11:03:43 +0100

    second commit

--- include/my_allocator.h
+++ include/my_allocator.h
@@ -1,23 +1,29 @@
-/*
 #ifndef MY_ALLOCATOR_H
 #define MY_ALLOCATOR_H
 
-#include <stddef.h>
-
-// Déclaration des fonctions
-void* my_malloc(size_t size);
-void my_free(void* ptr);
-
-#endif // ALLOCATOR_H
-*/
-
-#ifndef MY_ALLOCATOR_H
-#define MY_ALLOCATOR_H
 
 #include <stddef.h>
 
-// Déclaration des fonctions
+
+// Définition de la structure Block
+typedef struct Block {
+   size_t size;       // Taille du bloc
+   struct Block* next; // Pointeur vers le bloc suivant dans la liste libre
+   int free;          // Indicateur si le bloc est libre ou non
+} Block;
+
+
+#define PAGE_SIZE 4096
+#define BLOCK_SIZE sizeof(Block)
+#define NUM_CLASSES 10 // Nombre de classes pour les tailles de blocs (puissances de 2)
+
+
+// Fonctions de l'allocateur
 void* my_malloc(size_t size);
 void my_free(void* ptr);
-void coalesce_blocks(); // Fonction de coalescence des blocs libres
+void coalesce_free_blocks();
+
+
 #endif // MY_ALLOCATOR_H
+
+
